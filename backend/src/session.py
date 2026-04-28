311d4a0b289284e0bd12cf9cb34bc2c55e0a4389
AdirtKa <AdirtKa@yandex.ru>
2026-04-28 21:37:59 +1000

    make session

---
+++ backend/src/session.py
@@ -0,0 +1,15 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from src.config import settings
+
+engine = create_async_engine(settings.database_url, echo=settings.is_dev)
+async_session = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
